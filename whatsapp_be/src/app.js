9f4eb0f00909f1b4dac42488f925c823277edba6
youssef rhanem <youssefrhanem91@gmail.com>
2023-09-24 20:34:07 +0100

    whatsapp clone backend first setup

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -1,1 +1,16 @@
+import express from "express"
 
+
+
+// create express app
+const app = express();
+
+
+app.get('/', (req,res) => {
+    res.send('hello from server');
+});
+
+
+
+
+export default app;
--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,1 +1,13 @@
-console.log("welcome from a node js app");
+import app from "./app.js"
+import dotenv from "dotenv"
+
+// fotEnv config
+dotenv.config();
+
+// env Variables
+const PORT = process.env.PORT || 8000;
+
+
+app.listen(PORT, ()=> {
+    console.log(`server is listening at ${PORT}`);
+});
